931e3c22d364635f7ac9d2adcc824f44a4e96fdc
Rohan K Rajoli <rohankrajoli@gmail.com>
2026-05-31 02:32:08 +0530

    fix: pin node version and simplify vercel export

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,2 @@
 import app from "../server/index";
-
-export default async (req: any, res: any) => {
-  // This wrapper ensures that the Express app handles the request correctly in Vercel
-  return app(req, res);
-};
+export default app;
